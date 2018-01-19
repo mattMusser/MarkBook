fa4642f08e69d4179a3eb07a8f86b5a0ed13e8c7
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-19 08:22:45 -0600

    reconfigure devise mail settings and add confirmable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,10 +53,10 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Default url configuration for Devise
-  config.action_mailer.default_url_options = { :host => 'localhost', :port => 3000}
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.default_url_options = {:host => 'localhost', :port => 3000}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create Users
+15.times do
+	user = User.find_or_initialize_by(username: Faker::LordOfTheRings.character, email: Faker::Internet.safe_email) do |user|
+		user.password = Faker::Internet.password
+	end
+	user.skip_confirmation!
+	user.save!
+end
+
+# Create Admin User
+
+# Create Standard User
+standard = User.find_or_initialize_by(username: 'standard', email: 'standard@markshare.com') do |user|
+	user.password = 'password'
+	#user.role = 'standard'
+end
+standard.skip_confirmation!
+standard.save!
+users = User.all
+
+puts "Seeding Finished"
+puts "-.-.-.-.-.-.-.-.-"
+puts "#{User.count} total users"
