4c9522a797c66108a1c4005641fe561136984140
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-26 08:14:01 -0600

    code incoming controller create action

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -6,17 +6,29 @@ class IncomingController < ApplicationController
     # You put the message-splitting and business
     # magic here.
       # Find the user by using params[:sender]
-
+      @user = params[:sender]
       # Find the topic by using params[:subject]
-
+      @topic = params[:subject]
       # Assign the url to a variable after retrieving it from params["body-plain"]
-
+      @url = params["body-plain"]
       # Check if user is nil, if so, create and save a new user
-
+      if user.nil?
+        @user = User.new(params[:sender])
+        @user.save
+      end
       #Check if user the topic is nil, if so, create and save a new topic
-
+      if @topic.nil?
+        @topic = Topic.new(params[:subject])
+        @topic.save
+      end
       #now that you're sure you have a valid user and topic, build and save a new bookmark
+      @bookmark = @topic.bookmarks.build(bookmark_params)
 
     head 200
   end
-end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:url)
+  end
+end 
