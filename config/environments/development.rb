5584844ecd2bacf65e71e878e0269757833754d3
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-26 15:30:19 -0600

    fix routes and updating issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Default url configuration for Devise
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.smtp_settings = {:host => "localhost", :port => 1025}
   config.action_mailer.default_url_options = {:host => 'localhost', :port => 3000}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'users/create'
+  devise_for :users
+
   post :incoming, to: 'incoming#create'
 
   resources :topics do
     resources :bookmarks, except: [:index]
   end
 
-  devise_for :users
-
+  resources :users, only: [:index, :show]
   get 'faq' => 'welcome#faq'
 
   root 'welcome#index'
