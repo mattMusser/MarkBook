caccc4b5deec5ce0d5fb65437aaa526c763284e6
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-29 16:17:07 -0600

    configure mailer files

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+	default from: ENV['EMAIL']
+
+	def welcome_email
+		@user = params[:user]
+		@url 	= 'http://example.com/users/sign_in'
+		mail(to: @user.email, subject: 'Welcome to MarkShare')
+	end
+end
