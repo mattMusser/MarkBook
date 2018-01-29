05f8e2ae3d9f57a68f24796e139fef26e1bd4264
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-29 16:59:09 -0600

    add mailgun to mail initializer

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,21 @@
+#ActionMailer::Base.smtp_settings = {
+#	port:										587,
+#	address:								'smtp.sendgrid.net',
+#	user_name:							ENV['SENDGRID_LOGIN'],
+#	password:								ENV['SENDGRID_PASSWORD'],
+#	domain:									'mark-share.herokuapp.com',
+#	authentication:					'plain',
+#	enable_starttls_auto:		true
+#}
+
 ActionMailer::Base.smtp_settings = {
 	port:										587,
 	address:								'smtp.sendgrid.net',
-	user_name:							ENV['SENDGRID_LOGIN'],
-	password:								ENV['SENDGRID_PASSWORD'],
+	user_name:							ENV['MAILGUN_SMTP_LOGIN'],
+	password:								ENV['MAILGUN_SMTP_PASSWORD'],
 	domain:									'mark-share.herokuapp.com',
 	authentication:					'plain',
-	enable_starttls_auto:		true
+	content_type:						'text/html'
 }
 ActionMailer::Base.delivery_method = :smtp
 
