379924e2bcd075431766c14b3482c715a1cc08b6
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-30 09:54:30 -0600

    configure bookmarks policy

--- app/policies/bookmarks_policy.rb
+++ app/policies/bookmarks_policy.rb
@@ -1,50 +1,53 @@
-def initialize(user, record)
-	@user = user
-	@record = record
-end
+class ApplicationPolicy
+	attr_reader :user, :bookmark
 
-def index?
-	false
-end
-
-def show?
-	scope.where(:id => record.id).exists?
-end
-
-def create?
-	user.present?
-end
-
-def new?
-	create?
-end
-
-def update?
-	user.present? && (record.user == user)
-end
-
-def edit?
-	update?
-end
-
-def destroy?
-	user.present? && (record.user == user)
-end
-
-def scope
-	Pundit.policy_scope!(user, record.class)
-end
-
-class Scope
-	attr_reader :user, :scope
-
-	def initialize(user, scope)
+	def initialize(user, bookmark)
 		@user = user
-		@scope = scope
+		@record = record
 	end
 
-	def resolve
-		scope
+	def index?
+		user.present?
 	end
-end
+
+	def show?
+		scope.where(:id => bookmark.id).exists?
+	end
+
+	def create?
+		user.present?
+	end
+
+	def new?
+		create?
+	end
+
+	def update?
+		user.present? && (bookmark.user == user)
+	end
+
+	def edit?
+		update?
+	end
+
+	def destroy?
+		user.present? && (bookmark.user == user)
+	end
+
+	def scope
+		Pundit.policy_scope!(user, record.class)
+	end
+
+	class Scope
+		attr_reader :user, :scope
+
+		def initialize(user, scope)
+			@user = user
+			@scope = scope
+		end
+
+		def resolve
+			scope
+		end
+	end
 end
