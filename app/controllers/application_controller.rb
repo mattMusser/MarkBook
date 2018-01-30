c893abcc10b472fa860884c4d84856f54dfff67d
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-30 08:57:38 -0600

    remove env variable output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
-  puts "SENDGRID_LOGIN: #{ENV['SENDGRID_LOGIN']}"
-  puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
 
 end
