ebc669994bc4250f3728b27f12b3e408b4cc7cbb
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-30 08:52:46 -0600

    include Pundit in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
+
   puts "SENDGRID_LOGIN: #{ENV['SENDGRID_LOGIN']}"
   puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
 
