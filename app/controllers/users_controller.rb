7a6e7052da056e1285b98d3bbd74da6a52f1a24f
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-30 08:48:25 -0600

    remove username from edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :username)
+    params.require(:user).permit(:email)
   end
 end
