c06550173ae8d72dc41f4010d103dc4e15e672af
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-31 08:20:23 -0600

    create likes controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-
   devise_for :users
 
   post :incoming, to: 'incoming#create'
 
   resources :topics do
     resources :bookmarks, except: [:index]
+      resources :likes, only: [:index, :create, :destroy]
   end
+
 
   resources :users, only: [:index, :show]
   get 'faq' => 'welcome#faq'
