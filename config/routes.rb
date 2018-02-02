ff0ccbd0a77d4d92cd587415d88e99d96736510b
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-02-02 16:27:06 -0600

    create likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   post :incoming, to: 'incoming#create'
 
   resources :topics do
-    resources :bookmarks, except: [:index]
+    resources :bookmarks, except: [:index] do
+      resources :likes, only: [:index, :create, :destroy]
   end
 
   resources :users, only: [:index, :show]
