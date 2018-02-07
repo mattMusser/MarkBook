e37a3b200516212b5557c208383d8d7023a90919
Matt <webdevelopmentlyspeaking@gmail.com>
2018-02-06 21:49:14 -0600

    clean up gem file

--- Gemfile
+++ Gemfile
@@ -39,12 +39,9 @@ gem 'pundit'
 # Google Omniauthable
 gem 'omniauth-google'
 
-# Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202215141) do
+ActiveRecord::Schema.define(version: 20180207021945) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "url"
@@ -71,9 +71,13 @@ ActiveRecord::Schema.define(version: 20180202215141) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "provider"
+    t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end
